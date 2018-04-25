6ea2b95904326b7a9267b5e93c69cd5a97614a6c
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-04-25 16:08:57 -0700

    New Placeholder Fragments

---
+++ app/src/main/java/com/sidrk/travelentsearch/PlaceInfoFragment.java
@@ -0,0 +1,47 @@
+package com.sidrk.travelentsearch;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceInfoFragment extends Fragment {
+
+    private static final String TAG = "PlaceInfoFragment";
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public PlaceInfoFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceInfoFragment newInstance(int sectionNumber) {
+        PlaceInfoFragment fragment = new PlaceInfoFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_place_info, container, false);
+
+        //            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        //            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+        return rootView;
+    }
+}

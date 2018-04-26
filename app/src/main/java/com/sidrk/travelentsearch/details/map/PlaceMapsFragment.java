f2b05d9bd1906a65d1d1a208b7b465f4367bd735
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-04-26 11:28:47 -0700

    Map demo code

--- app/src/main/java/com/sidrk/travelentsearch/details/map/PlaceMapsFragment.java
+++ app/src/main/java/com/sidrk/travelentsearch/details/map/PlaceMapsFragment.java
@@ -9,7 +9,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.sidrk.travelentsearch.R;
 
 public class PlaceMapsFragment extends Fragment {
@@ -19,6 +22,9 @@ public class PlaceMapsFragment extends Fragment {
     private static final String ARG_PLACE_DETAIL = "placeDetailsJSON";
 
     private String placeDetailsJSON;
+
+    private MapView mMapView;
+    private GoogleMap googleMap;
 
     public PlaceMapsFragment() {
         // Required empty public constructor
@@ -48,12 +54,67 @@ public class PlaceMapsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_place_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_place_map, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                // googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+        return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
